5f74d127e77f9c323191c8a9f9c8d8aaaa9cfc32
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-06 08:16:50 -0800

    fix package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "HTTPRouter",
 	dependencies: [
-        .Package(url: "https://github.com/Zewo/CPOSIXRegex.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/POSIXRegex.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1)
 	]
 )
