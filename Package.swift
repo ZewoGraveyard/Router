fa0dc77080264a6a7a80184e2da48659e7435341
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-11 21:16:58 -0800

    remove useless import

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "Router",
 	dependencies: [
-        .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1)
 	]
 )
