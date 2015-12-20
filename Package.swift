36ed359f04cf559a8081b539d76c8c2017f9bde3
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 06:58:35 -0800

    fix 2015-12-18 toolchain SPM bug

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "Router",
 	dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
 	]
 )
