8d28555de3a779db6786ed2a692f7639e471ae1c
antonmes <antonmes@users.noreply.github.com>
2016-03-16 09:13:44 +0300

    Remove C deps

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Router",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/RegexRouteMatcher.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/Zewo/TrieRouteMatcher.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/RegexRouteMatcher.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/TrieRouteMatcher.git", majorVersion: 0, minor: 4),
     ]
 )
