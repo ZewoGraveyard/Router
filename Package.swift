2a0160a409c2c1d9b51e07ca093fa735af48027a
Donatas Stundys <donatas.stundys@gmail.com>
2016-05-08 03:12:03 +0300

    Support 05-03 snapshot (#10)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Router",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/RegexRouteMatcher.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/TrieRouteMatcher.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/RegexRouteMatcher.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/TrieRouteMatcher.git", majorVersion: 0, minor: 6),
     ]
 )
