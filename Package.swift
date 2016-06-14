0898063b156a92a7f22bbf071045daf382eba442
Tomohisa Takaoka <tomohisa.takaoka@gmail.com>
2016-06-14 14:59:31 +0900

    Bump to 0.8 (#11)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Router",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/RegexRouteMatcher.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/TrieRouteMatcher.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/RegexRouteMatcher.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/Zewo/TrieRouteMatcher.git", majorVersion: 0, minor: 8),
     ]
 )
